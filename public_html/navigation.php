9d5508e46e662b567913c4fce41e1abb5ed2961e
SC\drew.geisert <drew.geisert@my.simpson.edu>
2017-10-31 09:22:33 -0500

    Changed php files and added navigation

---
+++ public_html/navigation.php
@@ -0,0 +1,32 @@
+<nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+
+            <!-- Three line "hamburger" icon for the collapsed navigation -->
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+
+          <!-- Title on nav bar -->
+          <a class="navbar-brand" href="#">Simpson Pet Company</a>
+        </div>
+
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav navbar-left">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li class="active"><a href="hours_directions.php">Hours and Directions</a></li>
+            <li><a href="news.php">News</a></li>
+            <li><a href="pets_avaliable.php">Pets Available</a></li>
+            <li><a href="pets_sold.php">Pets Sold</a></li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="sign_up.php">Sign Up</a></li>
+            <li><a href="login.php">Log In</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
--- public_html/pets_avaliable.php
+++ public_html/pets_avaliable.php
@@ -24,38 +24,11 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-
-            <!-- Three line "hamburger" icon for the collapsed navigation -->
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-
-          <!-- Title on nav bar -->
-          <a class="navbar-brand" href="#">Simpson Pet Company</a>
-        </div>
-
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-left">
-            <li><a href="index.html">Home</a></li>
-            <li><a href="about.html">About</a></li>
-            <li><a href="hours_directions.html">Hours and Directions</a></li>
-            <li><a href="news.html">News</a></li>
-            <li class="active"><a href="pets_avaliable.html">Pets Available</a></li>
-            <li><a href="pets_sold.html">Pets Sold</a></li>
-          </ul>
-          <ul class="nav navbar-nav navbar-right">
-            <li><a href="sign_up.html">Sign Up</a></li>
-            <li><a href="login.html">Log In</a></li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+  
+     <?php
+        include 'navigation.php';
+    ?>
+    
     <div class="container-fluid">
       <section class="below_menu_container">
         <header>
